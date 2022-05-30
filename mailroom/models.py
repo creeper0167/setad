71561df7818b7d1e3bf7a55044465e97c322bf66
aliaberi <ali.aberi1995@gmail.com>
2022-05-30 23:04:48 +0430

    make person model

--- mailroom/models.py
+++ mailroom/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class person(models.models)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
