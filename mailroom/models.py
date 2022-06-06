bfa9d827b316f309080a12a75ba81b6744489161
aliaberi <ali.aberi1995@gmail.com>
2022-06-07 00:59:48 +0430

    added letter model to models file

--- mailroom/models.py
+++ mailroom/models.py
@@ -4,7 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class person(models.models)
+class person(models.model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     position = models.CharField(max_length=255)
+
+class letter(models.model):
+    author = models.IntegerField()           # author id from user table
+    registration_date = model.DateField()     
+    letter_number = model.CharField()
+    letter_date = model.CharField()
+    receiver_name = model.CharField()
+    receiver_post = model.CharField()
+    welcom = model.CharField()
+    letter_text = model.TextField()
+    Signatory_id = model.CharField()        # get from user table
+             
